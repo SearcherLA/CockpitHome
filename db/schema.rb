51de5bab2c0e59bce0074d9ce1b4181e1eff0476
SearcherLA <SearcherLA@ymail.com>
2018-02-11 22:44:55 +0100

    Team MVC erstellt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208143700) do
+ActiveRecord::Schema.define(version: 20180211174402) do
+
+  create_table "teams", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "teamname"
+    t.string "shortcut"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
