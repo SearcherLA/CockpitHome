3b9d0378357586ef0d4c8cb4558a45a9d8b1ab03
SearcherLA <SearcherLA@ymail.com>
2018-02-12 13:02:25 +0100

    teams layouts fertig

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,13 +2,11 @@ class TeamsController < ApplicationController
   before_action :set_team, only: [:show, :edit, :update, :destroy]
 
   # GET /teams
-  # GET /teams.json
   def index
     @teams = Team.all
   end
 
   # GET /teams/1
-  # GET /teams/1.json
   def show
   end
 
@@ -22,43 +20,29 @@ class TeamsController < ApplicationController
   end
 
   # POST /teams
-  # POST /teams.json
   def create
     @team = Team.new(team_params)
 
-    respond_to do |format|
       if @team.save
-        format.html { redirect_to @team, notice: 'Team was successfully created.' }
-        format.json { render :show, status: :created, location: @team }
+        redirect_to teams_url, notice: 'Team wurde erfolgreich erstellt.'
       else
-        format.html { render :new }
-        format.json { render json: @team.errors, status: :unprocessable_entity }
+        render :new
       end
-    end
   end
 
   # PATCH/PUT /teams/1
-  # PATCH/PUT /teams/1.json
   def update
-    respond_to do |format|
       if @team.update(team_params)
-        format.html { redirect_to @team, notice: 'Team was successfully updated.' }
-        format.json { render :show, status: :ok, location: @team }
+        redirect_to teams_url, notice: 'Team wurde erfolgreich geändert.'
       else
-        format.html { render :edit }
-        format.json { render json: @team.errors, status: :unprocessable_entity }
+        render :edit
       end
-    end
   end
 
   # DELETE /teams/1
-  # DELETE /teams/1.json
   def destroy
     @team.destroy
-    respond_to do |format|
-      format.html { redirect_to teams_url, notice: 'Team was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+      redirect_to teams_url, notice: 'Team wurde erfolgreich gelöscht.'
   end
 
   private
